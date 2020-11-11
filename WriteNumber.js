35372c311ad159de2042c6dc20af3ba20d6f539b
ijounjalkar <jounjalkarisha@gmail.com>
2020-11-12 02:25:41 +0530

    Added functionality to perform the writing of the number.

---
+++ WriteNumber.js
@@ -0,0 +1,25 @@
+var number = process.argv[2];
+var string = "Number is ";
+if(number == 0){
+    console.log(string + "Zero");
+}else if(number == 1){
+    console.log(string + "One");
+}else if(number == 2){
+    console.log(string + "Two");
+}else if(number == 3){
+    console.log(string + "Three");
+}else if(number == 4){
+    console.log(string + "Four");
+}else if(number == 5){
+    console.log(string + "Five");
+}else if(number == 6){
+    console.log(string + "Six");
+}else if(number == 7){
+    console.log(string + "Seven");
+}else if(number == 8){
+    console.log(string + "Eight");
+}else if(number == 9){
+    console.log(string + "Nine");
+}else{
+    console.log("Invalid input");
+}
